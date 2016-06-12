2dade6cc7201f4e897bbba4019650c6843bedab7
Neo Golightly <info@at-world.de>
2016-06-12 16:29:22 +0200

    Completed my first iOS framework!

---
+++ CartographyKit/CartographyKit.h
@@ -0,0 +1,21 @@
+//
+//  CartographyKit iOS.h
+//  CartographyKit iOS
+//
+//  Created by mhelmbre on 6/12/16.
+//
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for CartographyKit iOS.
+FOUNDATION_EXPORT double CartographyKitVersionNumber;
+
+//! Project version string for CartographyKit iOS.
+FOUNDATION_EXPORT const unsigned char CartographyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CartographyKit_iOS/PublicHeader.h>
+
+
